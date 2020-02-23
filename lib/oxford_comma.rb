b65339d91864dd2faf4049ca3dab01a44f8827ae
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-23 14:52:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   if array == 1 
     return array.size [0]
   elsif array.size == 2 
-    return array.join "and"
-  else array.size == 
+    return array.join ("and")
+  else array.size > 2 
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+end
 end
